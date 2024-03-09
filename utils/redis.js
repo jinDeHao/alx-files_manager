27e4af01fa5aa20a01ff90e1f1f78373def6af2b
jinDeHao <o.idhmaid@gmail.com>
2024-03-09 22:59:50 +0100

    task 0 requirement

--- utils/redis.js
+++ utils/redis.js
@@ -4,9 +4,10 @@ class RedisClient {
   constructor() {
     this.client = redis.createClient();
     this.client.on('error', (err) => {
-      console.error(err);
+      console.log(err);
     });
   }
+
   isAlive() {
     return this.client.connected;
   }
