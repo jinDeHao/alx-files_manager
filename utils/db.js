b04e4dd91d4b95e658b88fb52650d579222c1189
jinDeHao <o.idhmaid@gmail.com>
2024-03-10 05:16:06 +0000

    done with 1

--- utils/db.js
+++ utils/db.js
@@ -2,16 +2,13 @@ import { MongoClient } from 'mongodb';
 
 class DBClient {
   constructor() {
-    const DB_HOST =
-      process.env.DB_HOST === undefined ? 'localhost' : process.env.DB_HOST;
-    const DB_PORT =
-      process.env.DB_PORT === undefined ? '27017' : process.env.DB_PORT;
-    this.DB_DATABASE =
-      process.env.DB_DATABASE === undefined
-        ? 'files_manager'
-        : process.env.DB_DATABASE;
+    const DB_HOST = process.env.DB_HOST === undefined ? 'localhost' : process.env.DB_HOST;
+    const DB_PORT = process.env.DB_PORT === undefined ? '27017' : process.env.DB_PORT;
+    this.DB_DATABASE = process.env.DB_DATABASE === undefined
+      ? 'files_manager'
+      : process.env.DB_DATABASE;
     this.client = new MongoClient(
-      `mongodb://${DB_HOST}:${DB_PORT}/${this.DB_DATABASE}`
+      `mongodb://${DB_HOST}:${DB_PORT}/${this.DB_DATABASE}`,
     );
     this.client.connect();
   }
