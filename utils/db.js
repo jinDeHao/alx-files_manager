cbbb785ef6808c4966a288c17717bbe5c1a27883
jinDeHao <o.idhmaid@gmail.com>
2024-03-10 04:55:52 +0000

    db

---
+++ utils/db.js
@@ -0,0 +1,38 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const DB_HOST =
+      process.env.DB_HOST === undefined ? 'localhost' : process.env.DB_HOST;
+    const DB_PORT =
+      process.env.DB_PORT === undefined ? '27017' : process.env.DB_PORT;
+    this.DB_DATABASE =
+      process.env.DB_DATABASE === undefined
+        ? 'files_manager'
+        : process.env.DB_DATABASE;
+    this.client = new MongoClient(
+      `mongodb://${DB_HOST}:${DB_PORT}/${this.DB_DATABASE}`
+    );
+    this.client.connect();
+  }
+  isAlive() {
+    return this.client.isConnected();
+  }
+  async nbUsers() {
+    return new Promise(async (resolve) => {
+      const db = this.client.db(this.DB_DATABASE);
+      const userCollection = db.collection('users');
+      resolve(await userCollection.countDocuments());
+    });
+  }
+  async nbFiles() {
+    return new Promise(async (resolve) => {
+      const db = this.client.db(this.DB_DATABASE);
+      const userCollection = db.collection('files');
+      resolve(await userCollection.countDocuments());
+    });
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
