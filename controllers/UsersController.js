170d91722ff845186bcb99912812848269158fc8
jinDeHao <o.idhmaid@gmail.com>
2024-03-12 07:09:35 +0000

    missing messahe

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -6,13 +6,14 @@ class UsersController {
     console.log(req.body);
     try {
       const { email, password } = req.body;
-      if (email === undefined) return res.status(400).send('Missing email');
+      if (email === undefined)
+        return res.status(400).send({ error: 'Missing email' });
       if (password === undefined)
-        return res.status(400).send('Missing password');
+        return res.status(400).send({ error: 'Missing password' });
       const database = dbClient.client.db('files_manager');
       const collection = database.collection('users');
       const existsEmail = await collection.findOne({ email });
-      if (existsEmail) return res.status(400).send('Already exist');
+      if (existsEmail) return res.status(400).send({ error: 'Already exist' });
       const hpswrd = crypto.createHash('sha1').update(password).digest('hex');
       const document = await collection.insertOne({
         email: email,
