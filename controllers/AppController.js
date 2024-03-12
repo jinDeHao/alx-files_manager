327d6e3a759dc4da3f76feef3e4b0048c96c932e
jinDeHao <o.idhmaid@gmail.com>
2024-03-12 03:17:22 +0000

    2 plz

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,12 +1,13 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-const getStatus = () => {
-  return { redis: redisClient.isAlive(), db: dbClient.isAlive() };
-};
+class AppController {
+  static getStatus() {
+    return { redis: redisClient.isAlive(), db: dbClient.isAlive() };
+  }
+  static getStats = async () => {
+    return { users: await dbClient.nbUsers(), files: await dbClient.nbFiles() };
+  };
+}
 
-const getStats = async () => {
-  return { users: await dbClient.nbUsers(), files: await dbClient.nbFiles() };
-};
-
-export { getStatus, getStats };
+export default AppController;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import { getStatus, getStats } from '../controllers/AppController';
+import AppController from '../controllers/AppController';
 
 const router = express.Router();
 
 router.get('/status', (req, res) => {
-  res.status(200).send(JSON.stringify(getStatus()));
+  res.status(200).send(JSON.stringify(AppController.getStatus()));
 });
 
 router.get('/stats', async (req, res) => {
-  res.status(200).send(JSON.stringify(await getStats()));
+  res.status(200).send(JSON.stringify(await AppController.getStats()));
 });
 
 export default router;
