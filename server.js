3d5dc174c6381680190489d460e75ca77c503f8f
jinDeHao <o.idhmaid@gmail.com>
2024-03-12 02:57:52 +0000

    log

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 5000;
 app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
 
 export default app;
